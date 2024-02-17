bbe0a37601aaf32aefdec3fd4116125bfd30e625
Mak_Repo's <132384262+Makarand-sdw@users.noreply.github.com>
2024-02-17 20:27:36 +0530

    Update temp.py

--- temp/temp.py
+++ temp/temp.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = "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"
+openai.api_key = ""
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "C++ code for number is prime or not"}])
 print(completion.choices[0].message.content)
 
